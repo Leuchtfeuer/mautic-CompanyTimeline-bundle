1db94ff87136496933642c7fa7edd483b2a3d18f
lenonleite <lenonleite@gmail.com>
2025-07-24 19:49:06 +0100

    - remove entity from services

--- Config/services.php
+++ Config/services.php
@@ -17,8 +17,6 @@ return function (ContainerConfigurator $configurator): void {
 
     $services->load('MauticPlugin\\CompanyTimelineBundle\\', '../')
         ->exclude('../{'.implode(',', array_merge(MauticCoreExtension::DEFAULT_EXCLUDES, $excludes)).'}');
-    $services->load('MauticPlugin\\CompanyTimelineBundle\\Entity\\', '../Entity/*Repository.php')
-        ->tag(Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\ServiceRepositoryCompilerPass::REPOSITORY_SERVICE_TAG);
 
     $services->alias('mautic.company_timeline.model.custom_company_event_log', MauticPlugin\CompanyTimelineBundle\Model\CustomCompanyEventLogModel::class);
     $services->alias('mautic.email.model.stat', Mautic\EmailBundle\Model\EmailStatModel::class);
