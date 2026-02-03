2de9495db8a5159a7466c448546354423d2efd6d
Ekkehard Gümbel <43146234+ekkeguembel@users.noreply.github.com>
2026-02-03 10:45:39 +0100

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'name'        => 'Leuchtfeuer Digital Marketing GmbH',
     'description' => 'Add a detailed history tab for Companies',
-    'version'     => '6.1.0',
+    'version'     => '6.1.1',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
     'services'    => [
         'integrations' => [
