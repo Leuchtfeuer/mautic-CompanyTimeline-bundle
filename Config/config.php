af3dc72d65ad8eb0e7a5d36417639663ee2584dc
Madlen <m.friedrich@leuchtfeuer.com>
2026-07-21 15:50:18 +0200

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'name'        => 'Leuchtfeuer Digital Marketing GmbH',
     'description' => 'Add a detailed history tab for Companies',
-    'version'     => '5.0.0',
+    'version'     => '5.0.1',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
     'services'    => [
         'integrations' => [
